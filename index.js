dc5aa153695ccba8a1e39ac69b5d2ec9c182c193
Mari Topel <maritopel@gmail.com>
2025-06-09 14:40:46 -0500

    made it so the numbers are put into the bank. but it does not look like the screenshot so i did it wrong. figuring out how to add spaced numbers and putting it into a box

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
+// input for numbers
 const inputNumber = document.createElement("input");
-
 inputNumber.setAttribute("type", "number");
-
 inputNumber.id = "myNumberInput";
 inputNumber.setAttribute("placeholder", "type numbers here");
-
 document.body.appendChild(inputNumber);
 
+// button for click to input numbers
 const button = document.createElement("button");
 button.textContent = "Submit Number";
 document.body.appendChild(button);
+
+// making the bank
+const bank = document.createElement("h3");
+bank.textContent = "Banked";
+document.body.appendChild(bank);
+const showBank = document.createElement("ul");
+document.body.appendChild(showBank);
+
+const bankList = [];
 
 button.addEventListener("click", () => {
   const number = parseFloat(inputNumber.value);
 
   if (isNaN(number)) {
     alert("not a valid number");
-  } else {
-    alert(`You entered ${number}`);
+    return;
   }
+
+  bankList.push(number);
+
+  const listItem = document.createElement("li");
+  listItem.textContent = number;
+  showBank.appendChild(listItem);
+
+  inputNumber.value = "";
 });
